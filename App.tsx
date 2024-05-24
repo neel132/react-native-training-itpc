b5d6cb6478682d8b2f7ee055261098ef87c37e41
= <=>
2024-05-24 16:06:30 +0530

    switching between navigators

--- App.tsx
+++ App.tsx
@@ -1,26 +1,48 @@
-import 'react-native-gesture-handler';
-import React from 'react';
-import { SafeAreaView } from 'react-native';
-import Home from './src/screens/Home/Home.func';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import About from './src/screens/About/About';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import "react-native-gesture-handler";
+import React from "react";
+import { SafeAreaView } from "react-native";
+import Home from "./src/screens/Home/Home.func";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import About from "./src/screens/About/About";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import Login from "./src/screens/Login/Login";
+
+// const Tab = createBottomTabNavigator();
+
+// function MyTabs() {
+//   return (
+//     <Tab.Navigator>
+//       <Tab.Screen name="Home" component={HomeScreen} />
+//       <Tab.Screen name="Settings" component={SettingsScreen} />
+//     </Tab.Navigator>
+//   );
+// }
 
 const Stack = createNativeStackNavigator();
-// const Drawer = createDrawerNavigator();
+const Drawer = createDrawerNavigator();
 function App(): JSX.Element {
-
+  const isUserLoggedIn = true;
   return (
     <NavigationContainer>
-      {/* <Drawer.Navigator>
-        <Drawer.Screen name="Home" component={Home} />
-        <Drawer.Screen name="AboutUs" component={About} />
-      </Drawer.Navigator> */}
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="AboutUs" component={About} />
-      </Stack.Navigator>
+      {isUserLoggedIn ? (
+        <Drawer.Navigator initialRouteName="Home">
+          <Drawer.Screen
+            name="Home"
+            component={Home}
+            options={{ title: "Dashboard" }}
+          />
+          <Drawer.Screen
+            name="AboutUs"
+            component={About}
+            options={{ title: "About" }}
+          />
+        </Drawer.Navigator>
+      ) : (
+        <Stack.Navigator>
+          <Stack.Screen name="login" component={Login} />
+        </Stack.Navigator>
+      )}
     </NavigationContainer>
   );
 }
---
+++ src/screens/Login/Login.tsx
@@ -0,0 +1,8 @@
+import { Text } from "react-native";
+
+const Login = () => {
+  return (
+    <Text>This is a login Screen</Text>
+  );
+}
+export default Login;
